d1605aafcae2f448e4f2abe48d3cfb006447d711
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-18 23:23:04 +0700

    config database

--- modes/pg_config.js
+++ modes/pg_config.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pg_con = new Pool({
-    user: 'adylywirgshulz',
-    host: 'ec2-23-20-140-229.compute-1.amazonaws.com',
-    database: 'de575n8rtbdrrv',
-    password: '3f398292682d08081097e03c9e575f9e74003a752bc4638687cd4b502b91d1e2',
+    user: 'dxuiijktystkhz',
+    host: 'ec2-18-209-78-11.compute-1.amazonaws.com',
+    database: 'd2avpvj0chc9id',
+    password: 'c175916765e4903f34c87edf6b4e2867262260035d03fdfd05b911320594fa66',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
